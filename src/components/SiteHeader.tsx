60ca71735918e35d0201799f13429e29b0fbb61e
ntandomadonsela <ntandomadonsela168@gmail.com>
2026-09-03 18:37:06 +0200

    Update SiteHeader.tsx

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,7 +1,7 @@
 import { Link } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 
-import nesmaLogo from "nesai-nova-edge/blob/main/src/assets/nesma%20logo.png";
+import nesmaLogo from "";
 import novaLogo from "@/assets/nesai-nova-logo.png.asset.json";
 
 type NavItem = { label: string; href: string };
